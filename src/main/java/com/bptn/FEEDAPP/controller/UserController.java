aaab98c7401dbe45c482cdf88d4b987fb260bf03
BOSTONE069 <bostoneochieng69@gmail.com>
2024-05-01 18:11:35 +0300

    DATA BASE CONTROL

--- src/main/java/com/bptn/FEEDAPP/controller/UserController.java
+++ src/main/java/com/bptn/FEEDAPP/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.bptn.FEEDAPP.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.bptn.FEEDAPP.service.UserService;
 
 public class UserController {
+	
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	UserServices userService;
+	
+	@GetMapping("/")
+	public List<UserBean> listUsers() {
+		logger.debug("The listUsers() method was invoked!");
+		return this.userService.listUsers();
+	}
 
 }
